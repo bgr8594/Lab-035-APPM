f076fc63f459d58fee48e2359d9135ee5613c936
bgr8594 <bgr8594@gmail.com>
2024-02-12 21:12:21 -0600

    Practica 3
    Pagina de alumnos INPUT, OUTPUT Event Emiter, comunicacion entre componentes hijo y padre

---
+++ ionic-basic/src/app/componentes/table-alumno/table-alumno.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Alumno } from 'src/app/interface/alumno';
+
+@Component({
+  selector: 'app-table-alumno',
+  templateUrl: './table-alumno.component.html'
+})
+export class TableAlumnoComponent  implements OnInit {
+
+  @Input() alumnosList:Alumno[] =[];
+  @Output() eliminar: any = new EventEmitter<Alumno>();
+  @Output() editar:  any = new EventEmitter<number>();
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
